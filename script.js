4918e2786396c5fde961df7829b32ac7502d6a77
Glitch (personality-quiz-starter-code) <none>
2020-07-26 02:07:50 +0000

    🍍🏩 Checkpoint ./script.js:19026581/79

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 //This is where you will define the variables you will be using in your project.
 //#TODO: Create four variables to track each possible quiz outcome
 var questionCount = 0;
-var fun1Score = 0;
-var creative2Score = 0;
-var outgoing3Score = 0;
-var original4Score = 0;
+var funScore = 0;
+var creativeScore = 0;
+var outgoingScore = 0;
+var originalScore = 0;
 
 
 //#TODO: Use the DOM to create variables for the first quiz question.
@@ -20,29 +20,29 @@ var q1a4 = document.getElementById("q1a4");
 
 
 //#TODO: Add Event Listeners to your answer choice variables.
-q1a1.addEventListener("click", fun1);
-q1a2.addEventListener("click", fun1);
-q1a3.addEventListener("click", fun1);
-q1a4.addEventListener("click", fun1);
+q1a1.addEventListener("click", outgoingScore);
+q1a2.addEventListener("click", originalScore);
+q1a3.addEventListener("click", funScore);
+q1a4.addEventListener("click", creativeScore);
 
 
 //#TODO: Define quiz functions here
-function fun1(){
-fun1Score = "fun1Score + 1";  
+function fun(){
+funScore = "funScore + 1";  
 questionCount = "questionCount + 1"; 
 }
 
-function creative2(){
- creative2Score = "creative2Score + 1";
+function creative(){
+ creativeScore = "creativeScore + 1";
  questionCount = "questionCount + 1"; 
 }
 
-function outgoing3(){
- outgoing3Score = "outgoing3Score + 1";
+function outgoing(){
+ outgoingScore = "outgoingScore + 1";
  questionCount =  "questionCount + 1"; 
 }
 
-function original4(){
- original4Score = "original4Score + 1";
+function original(){
+ originalScore = "originalScore + 1";
  questionCount =  "questionCount + 1"; 
 }
